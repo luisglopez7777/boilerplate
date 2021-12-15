1c4411eeec553d1048e0e3a84d59f8b3a6f7e273
luisglopez7777 <luis.lopez@leftfieldlabs.com>
2021-12-15 17:10:57 -0600

    first boilerplate

---
+++ README.md
@@ -0,0 +1,1 @@
+## Simple boilerplate for react projects
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div>
+      <h1>luisglopez7777</h1>
+      <p>Let's cook</p>
+    </div>
+  );
+}
